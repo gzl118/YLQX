57bb2c726ea978ae1a644bf45eaefaa9cbf68c34
gzl <343466439@qq.com>
2019-12-27 16:47:00 +0800

    增加初次打开时，执行报警存储过程

--- MedicalApparatusManage/Controllers/WebMainController.cs
+++ MedicalApparatusManage/Controllers/WebMainController.cs
@@ -38,6 +38,7 @@ namespace MedicalApparatusManage.Controllers
 
         public ActionResult Login()
         {
+            CommonDomain.ExecProc();
             return View();
         }
 
---
+++ MedicalApparatusManage/Domain/CommonDomain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MedicalApparatusManage.Domain
+{
+    public class CommonDomain
+    {
+        public static void ExecProc()
+        {
+            using (MedicalApparatusManageEntities hContext1 = new MedicalApparatusManageEntities())
+            {
+                try
+                {
+                    hContext1.proc_get_bjd();
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+    }
+}
